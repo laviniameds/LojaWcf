dfebe9a958d3f67712c30267d638e6d2bdb39494
laviniameds <laviniameds@icloud.com>
2016-06-01 22:17:44 -0300

    quase pronto.

---
+++ LojaWcf/IService1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LojaWcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        List<Fabricante> FabricanteSelect();
+
+        [OperationContract]
+        void FabricanteInsert(int id, string descricao);
+
+        [OperationContract]
+        void FabricanteUpdate(int id, string descricao);
+
+        [OperationContract]
+        void FabricanteDelete(int id);
+
+        [OperationContract]
+        List<Veiculo> VeiculoSelectDisponiveis();
+
+        [OperationContract]
+        void VeiculoInsert(int id, string modelo, int ano, int idFab, DateTime dataCompra,
+            decimal valorCompra, decimal precoVenda, DateTime dataVenda, decimal valorVenda);
+
+        [OperationContract]
+        void VeiculoUpdate(int id, string modelo, int ano, int idFab, DateTime dataCompra,
+            decimal valorCompra, decimal precoVenda, DateTime dataVenda, decimal valorVenda);
+
+        [OperationContract]
+        void VeiculoDelete(int id);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ LojaWcf/Service1.svc.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LojaWcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public List<Fabricante> FabricanteSelect()
+        {
+            LojaDataContext dc = new LojaDataContext();
+            var ret = from f in dc.Fabricantes select f;
+            if (ret.Count() == 0) return null;
+            return ret.ToList();
+        }
+
+        public void FabricanteInsert(int id, string descricao)
+        {
+            LojaDataContext dc = new LojaDataContext();
+            Fabricante f = new Fabricante { Id = id, Descricao = descricao };
+            dc.Fabricantes.InsertOnSubmit(f);
+            dc.SubmitChanges();
+        }
+
+        public void FabricanteUpdate(int id, string descricao)
+        {
+            LojaDataContext dc = new LojaDataContext();
+            var ret = from f in dc.Fabricantes where f.Id == id select f;
+            if (ret.Count() >= 0)
+            {
+                Fabricante f = ret.Single();
+                f.Descricao = descricao;
+                dc.SubmitChanges();
+            }
+        }
+
+        public void FabricanteDelete(int id)
+        {
+            LojaDataContext dc = new LojaDataContext();
+            var ret = from f in dc.Fabricantes where f.Id == id select f;
+            if (ret.Count() >= 0)
+            {
+                Fabricante f = ret.Single();
+                dc.Fabricantes.DeleteOnSubmit(f);
+                dc.SubmitChanges();
+            }
+        }
+
+        public List<Veiculo> VeiculoSelectDisponiveis()
+        {
+            LojaDataContext dc = new LojaDataContext();
+            var ret = from v in dc.Veiculos
+                      where !v.DataVenda.HasValue select v;
+            if (ret.Count() == 0) return null;
+            return ret.ToList();
+        }
+
+        public void VeiculoInsert(int id, string modelo, int ano, int idFab, 
+            DateTime dataCompra, decimal valorCompra, decimal precoVenda, DateTime dataVenda,
+            decimal valorVenda)
+        {
+            LojaDataContext dc = new LojaDataContext();
+            Veiculo v = new Veiculo
+            {
+                Id = id,
+                Modelo = modelo,
+                Ano = ano,
+                IdFabricante = idFab,
+                DataCompra = dataCompra,
+                ValorCompra = valorCompra,
+                PrecoVenda = precoVenda,
+                DataVenda = dataVenda,
+                ValorVenda = valorVenda
+            };
+            dc.Veiculos.InsertOnSubmit(v);
+            dc.SubmitChanges();
+        }
+
+        public void VeiculoUpdate(int id, string modelo, int ano, int idFab,
+            DateTime dataCompra, decimal valorCompra, decimal precoVenda, DateTime dataVenda,
+            decimal valorVenda)
+        {
+            LojaDataContext dc = new LojaDataContext();
+            var ret = from v in dc.Veiculos where v.Id == id select v;
+            if (ret.Count() >= 0)
+            {
+                Veiculo v = ret.Single();
+                v.Modelo = modelo;
+                v.Ano = ano;
+                v.IdFabricante = idFab;
+                v.DataCompra = dataCompra;
+                v.ValorCompra = valorCompra;
+                v.PrecoVenda = precoVenda;
+                v.DataVenda = dataVenda;
+                v.ValorVenda = valorVenda;
+                dc.SubmitChanges();
+            }
+        }
+
+        public void VeiculoDelete(int id)
+        {
+            LojaDataContext dc = new LojaDataContext();
+            var ret = from v in dc.Veiculos where v.Id == id select v;
+            if (ret.Count() >= 0)
+            {
+                Veiculo v = ret.Single();
+                dc.Veiculos.DeleteOnSubmit(v);
+                dc.SubmitChanges();
+            }
+        }
+    }
+}
